92a93e92de303183d12c615faae8f1a9eae0ca70
Sean I. Young <siyoung@wasabi.csail.mit.edu>
2024-03-24 23:09:54 -0400

    entrypoint shell scripts for train and test

---
+++ feta3d_svr_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -x
+python feta3d_svr_test.py
---
+++ feta3d_svr_train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+cat "$0" >&2 
+model="flow_SNet3d0_384" # "flow_UNet2d" #"flow_UNet2d"
+loss="l22_loss_affine_invariant"
+dataset="feta3d0_4_svr"
+train_metrics="MeanL22LossInvariant MeanL21LossInvariant"
+valid_metrics="MeanL22LossInvariant MeanL21LossInvariant"
+tests_metrics="MeanL22LossInvariant MeanL21LossInvariant"
+batch_size=1
+mode="--train" # --load $load" # --resume --load $load"
+seed=0
+
+remarks="$dataset"_"$model"_"$loss"_bulktrans0_bulkrot180_trans10_rot20_250k_cvpr #unmasked_pe_trans_random_zoom #_no_skip
+python train.py --trainee segment --dataset $dataset --batch_size $batch_size --valid_batch_size $batch_size --loss $loss --valid_metrics $valid_metrics --tests_metrics $tests_metrics --network $model --padding 0 --max_steps 250000 --limit_train_batches 20000 --optim adam --lr_start 1e-4 --momentum 0.90 --decay 0.0000 --schedule poly --val_check_interval 1.0 --monitor val_loss --monitor_mode min --seed $seed --remarks $remarks $mode
