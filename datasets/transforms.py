468c2bda51e0018b94ca512a995a036d7248718a
Sean I Young <siy0@kale.nmr.mgh.harvard.edu>
2024-04-07 15:01:32 -0400

    update splat-slice to the latest cornucopia API

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -74,29 +74,51 @@ class RandAffine3dSlice:
         
         return img0, flow
 
-class RandAffine3dSliceSplat(RandAffine3dSlice):
-    def __init__(self, spacing=4, subsample=1, slice=1, nodes=None, size=None):
-        super().__init__(spacing=spacing, subsample=subsample, slice=slice)
-        self.mult = cc.MaybeTransform(cc.RandomMultFieldTransform(), 0.5)
-        self.crop = cc.fov.RandomPatchTransform(patch_size=size)
-        self.base = cc.RandomSlicewiseAffineTransform(nodes=nodes, spacing=spacing, subsample=subsample, slice=slice, shears=0, zooms=0)
+class RandAffine3dSliceSplat:
+    def __init__(self, spacing=1, translations=0.10, rotations=20, bulk_translations=0, bulk_rotations=180, zooms=0, subsample=1, slice=1, nodes=(8,16), shots=2, augment=True, noise=False, X=3):
+        self.slice = slice if isinstance(slice, (tuple, list)) else [slice]
+        # self.crop = cc.fov.PatchTransform(192)
+        self.flip = cc.fov.RandomFlipTransform(axes=[-3]) #+ cc.fov.PatchTransform(192)
+        self.zoom = cc.MaybeTransform(cc.RandomAffineTransform(translations=0, rotations=0, shears=0, zooms=zooms, iso=True), 0.9)
+        self.base = [cc.RandomSlicewiseAffineTransform(nodes=nodes, shots=shots, spacing=spacing, subsample=subsample, slice=s, translations=translations, rotations=rotations, 
+                                                       bulk_translations=bulk_translations, bulk_rotations=bulk_rotations, shears=0, zooms=0) for s in self.slice]
+        self.mult = cc.MaybeTransform(cc.RandomGaussianNoiseTransform(sigma=0.01), 0.5) \
+            # + cc.MaybeTransform(cc.RandomGammaTransform(value=(0.7, 1.5)), 0.5) + cc.MaybeTransform(cc.RandomMultFieldTransform(), 0.5)
+        self.bound = BoundingBox3d(spacing=spacing, subsample=subsample)
+        self.augment = augment #add_noise
+        self.noise = noise
+        self.X = X
 
     def __call__(self, img1, seg1):
         if img1.ndim == 5:
             img1, seg1 = zip(*[self(img1[i], seg1[i]) for i in range(img1.shape[0])])
             return torch.stack(img1, 0), torch.stack(seg1, 0)
 
-        xform = self.base.get_parameters(img1)
-        matrix, flow, warp = xform.get_parameters(img1)
+        numstacks = 1 #torch.randint(1, len(self.slice) + 1, [1]).item()
+        img1 = (img1.clamp(min=0.1) - 0.1) * (1 / 0.9)
+        seg1 = ((img1 > 0) | (seg1 > 0)).float()
+        # img1, seg1 = self.crop(img1, seg1)
+        img1, seg1 = self.flip(img1, seg1) if self.augment else (img1, seg1)
+        xform = self.zoom.make_final(img1)
+        img1, seg1 = xform(img1, seg1) if self.augment else (img1, seg1)
 
-        img0 = xform.forward_with_parameters(img1, (matrix, flow, warp))
-        img0 = self.mult(img0)
+        xform = [self.base[i].make_final(img1) for i in range(numstacks)]
+        img0 = torch.cat([xform[i](img1) for i in range(numstacks)], 1)
+        seg0 = torch.cat([xform[i](seg1).gt(0).float() for i in range(numstacks)], 1)
+        # img0 = self.mult(img0) if self.noise else img0
+        flow = torch.cat([xform[i].flow for i in range(numstacks)], 1) # 
+        # img0, flow = self.bound(torch.cat([img0, seg0]), torch.cat([flow, seg0]), mask=seg0)
+        img0, flow = torch.cat([img0, seg0]), torch.cat([flow, seg0])
+        grid = [torch.arange(0, img0.shape[d], dtype=torch.float, device=img0.device) for d in range(1, img0.ndim)]
+        warp = torch.movedim(flow[:3] + torch.stack(torch.meshgrid(grid, indexing='ij'), 0), 0, -1)
 
         img0 = interpol.grid_push(img0, warp, bound=1, extrapolate=True)
-        ones = interpol.grid_count(warp, bound=1, extrapolate=True)[None] + 1e-4
-        img0, img1, ones = self.crop(img0, img1, ones)
+        ones = interpol.grid_count(warp, bound=1, extrapolate=True)[None] + 1e-8
+        # breakpoint()
+        img0, img1 = self.bound(img0/ones, torch.cat([img1, seg1], 0), mask=seg1)
+        #torch.cat([img0, seg0]), torch.cat([flow, seg0]), mask=seg0)
 
-        return torch.cat([img0 / ones], 0), img1 # - img0
+        return img0, img1 #torch.cat([img0 / ones], 0), img1 # - img0
 
 class ScaleZeroOne():
     def __init__(self, sig_gamma_sq=0.0):
